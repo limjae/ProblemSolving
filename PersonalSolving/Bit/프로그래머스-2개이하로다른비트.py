b8179587cc73fee972a7beb73776ef66cd6d4113
limjae <limjae96@gmail.com>
2022-06-09 22:27:00 +0900

    6/9

---
+++ PersonalSolving/Bit/프로그래머스-2개이하로다른비트.py
@@ -0,0 +1,13 @@
+# https://programmers.co.kr/learn/courses/30/lessons/77885
+def solution(numbers):
+    answer = []
+    for num in numbers:
+        if num % 2 == 0:
+            answer.append(num + 1)
+        else:
+            last = bin(num).rfind('0')
+            if last == 0:
+                answer.append(num + 2**(len(bin(num)) - 3))
+            else:
+                answer.append(num + 2**(len(bin(num))  - last - 2))
+    return answer
