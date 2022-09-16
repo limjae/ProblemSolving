e5cc347bfd6a3ff478dd5f37af001d1a806469fc
limjae <limjae96@gmail.com>
2022-09-16 14:47:37 +0900

    9/16

--- Python/KaKao/파괴되지않은건물.py
+++ Python/KaKao/파괴되지않은건물.py
@@ -1,7 +1,7 @@
 # 누적합 연산 문제...
 def solution(board, skill):
     answer = 0
-    acc_board = [[0 for _ in range(len(board[0])+1)] for _ in range(len(board)+1)]
+    acc_board = [[0 for _ in range(len(board[0]) + 1)] for _ in range(len(board) + 1)]
 
     for cur_skill in skill:
         s_type, r1, c1, r2, c2, degree = cur_skill
@@ -10,17 +10,17 @@ def solution(board, skill):
             degree = -degree
 
         acc_board[r1][c1] += degree
-        acc_board[r1][c2+1] -= degree
-        acc_board[r2+1][c1] -= degree
-        acc_board[r2+1][c2+1] += degree
+        acc_board[r1][c2 + 1] -= degree
+        acc_board[r2 + 1][c1] -= degree
+        acc_board[r2 + 1][c2 + 1] += degree
 
     for x in range(len(board[0])):
         for y in range(len(board)):
-            acc_board[y][x+1] +=  acc_board[y][x]
+            acc_board[y][x + 1] += acc_board[y][x]
 
     for y in range(len(board)):
         for x in range(len(board[0])):
-            acc_board[y+1][x] +=  acc_board[y][x]
+            acc_board[y + 1][x] += acc_board[y][x]
 
     # print(board)
     # print(acc_board)
