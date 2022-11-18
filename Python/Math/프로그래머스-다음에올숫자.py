903b121139bb334b463b03f27388dac415fd267b
limjae <limjae96@gmail.com>
2022-11-18 17:37:03 +0900

    11/18

---
+++ Python/Math/프로그래머스-다음에올숫자.py
@@ -0,0 +1,4 @@
+def solution(common):
+    if common[2] - common[1] == common[1] - common[0]:
+        return common.pop() + common[1] - common[0]
+    return common.pop() * (common[1] // common[0])
