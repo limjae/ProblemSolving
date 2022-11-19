969fe0fa958f4246b2d3b76497bd73a0e799b373
limjae <limjae96@gmail.com>
2022-11-19 13:23:22 +0900

    11/19 스타일 수정

--- Python/BruteForce/프로그래머스- 안전지대.py
+++ Python/BruteForce/프로그래머스- 안전지대.py
@@ -1,16 +1,8 @@
 def solution(board):
     check_range = [
-        [0, 1],
-
-        [1, 1],
-        [1, 0],
-        [1, -1],
-
-        [0, -1],
-
-        [-1, -1],
-        [-1, 0],
-        [-1, 1]
+        [1, -1], [1, 0], [1, 1],
+        [0, -1], [0, 0], [0, 1],
+        [-1, -1], [-1, 0], [-1, 1]
     ]
     answer = 0
     n = len(board)
@@ -18,14 +10,10 @@ def solution(board):
     for x in range(n):
         for y in range(n):
             chk = True
-            if board[y][x] == 1:
-                continue
-
             for d in check_range:
                 dx, dy = d
                 if -1 < x + dx < n and -1 < y + dy < n and board[y + dy][x + dx] == 1:
                     chk = False
-
             if chk:
                 answer += 1
 
